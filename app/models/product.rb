d679251a354664069c79a683c1e152661c998580
AR iMac <alex@brandreaktor.com>
2015-05-08 15:18:24 -0500

    Updated store view

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,6 +10,11 @@ class Product < ActiveRecord::Base
 		with: %r{\.(gif|jpg|png)\Z}i,
 		message: 'must be a URL for GIF, JPG or PNG image.'
 	}
+
+# Used for cache function in store index
+	def self.latest
+		Product.order(:updated_at).last
+	end
 	
 
 end
