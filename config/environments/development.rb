fd2b9291b61e1458c204a1ad8afa404f54b56196
AR iMac <alex@brandreaktor.com>
2015-05-11 16:41:19 -0500

    Mailer Funcionallity

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,20 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+
+  # Mailer Configuration
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :user_name            => "user@gmail.com",
+    :password             => "passs",
+    :authentication       => :plain,
+    :enable_starttls_auto => true
+  }
+
 end
